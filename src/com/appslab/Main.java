7dbee62b004ff28650f540158e4cc4c48b9a8842
simonheglasik <simonheglasik@gmail.com>
2020-09-24 09:09:37 +0200

    equalityValue

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,19 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        // write your code here
+        System.out.println(EqualValue(3,3,2));
+    }
+    public static int EqualValue(int a , int b, int c){
+        int equal = 0;
+        int numbers[] = {a,b,c};
+        for (int i = 0;i < numbers.length;i++){
+            for (int si = 0;si < numbers.length;si++){
+                if(numbers[i] == numbers[si] && i != si){
+                    equal++;
+                }
+            }
+        }
+        return equal;
     }
 }
