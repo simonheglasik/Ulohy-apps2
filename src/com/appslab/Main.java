4e1594c151d3fd1ccb2bccd21de2c1a17cef8bb3
simonheglasik <simonheglasik@gmail.com>
2020-09-24 09:29:29 +0200

    IsTriangle

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(IsTriangle(12,12,4));
+    }
+    public static boolean IsTriangle(int a,int b, int c){
+        return a + b > c &&  a+ c > b && c + b > a;
     }
 }
