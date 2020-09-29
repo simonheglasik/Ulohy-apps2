dd5652d9ab55ff5652229748fadc4503997f8351
z18ista05 <simonheglasik@gmail.com>
2020-09-29 09:33:54 +0200

    EqualSlice

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(equalSlices(200,50,2));
+    }
+    public static boolean equalSlices(int slices,int people,int sliceForEach){
+        return slices % people * sliceForEach == 0;
     }
 }
