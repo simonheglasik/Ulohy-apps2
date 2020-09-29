f69c58f83c9078efbe2191086bc030f57e9f1a21
z18ista05 <simonheglasik@gmail.com>
2020-09-29 09:58:59 +0200

    Palindrome

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -4,5 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(isPalindrome("mom"));
+
+
+    }
+    public static boolean isPalindrome(String word)
+    {
+        String s = "";
+       for (int i = word.length() - 1;i >= 0; i--)
+       {
+              s = s + word.charAt(i);
+       }
+       System.out.println(s);
+       System.out.println(word);
+       return s.equals(word);
     }
 }
