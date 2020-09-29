0b9f65b52b128ab67e5fb64a34cfd04b9dde1d64
z18ista05 <simonheglasik@gmail.com>
2020-09-29 09:05:42 +0200

    multipleByLenght

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,28 @@
+
 package com.appslab;
 
-public class Main {
 
-    public static void main(String[] args) {
+public class Main
+{
+
+    public static void main(String[] args)
+    {
         System.out.println("Hello AppsLab 2020! haa");
+        int[] numbers = {2,3,2,5,1};
+        numbers = multipleByLenght(numbers);
+        for ( int number: numbers
+             ) {
+            System.out.println(number);
+        }
+
+    }
+
+    public static int[] multipleByLenght(int[] numbers)
+    {
+        for (int number = 0; number < numbers.length;number++)
+        {
+            numbers[number] = numbers[number] * numbers.length;
+        }
+        return numbers;
     }
 }
