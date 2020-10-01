b36450551ccda435a03240c02c26cf4559161503
z18ista05 <simonheglasik@gmail.com>
2020-10-01 08:28:01 +0200

    game rock paper scissors

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -7,19 +7,29 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello AppsLab 2020! haa");
-        Game();
+        while (true)
+            Game();
     }
     private static Scanner in = new Scanner(System.in);
     public static void Game()
     {
        var rnd = new Random();
-       String[] tools = {"rock","paper","scissors"};
+       String[] tools = {"rock","scissors","paper"};
        for (int i = 0;i < tools.length;i++)
         System.out.println(i + 1 + ". " + tools[i]);
        int a = in.nextInt();
+       a = a - 1;
        int b = rnd.nextInt(3);
-       //System.out.println(b + 1);
+       System.out.println(tools[b]);
+
        if(a == b)
            System.out.println("TIE");
+       else
+           {
+                if(a == b - 1 || a == b + 2)
+                    System.out.println("player won");
+                else
+                    System.out.println("player lost");
+           }
     }
 }
