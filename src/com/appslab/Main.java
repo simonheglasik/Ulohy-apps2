3fa5f0b790649f506f1458b2393eac2a7074d726
simon heglasik <simonheglasik@gmail.com>
2020-10-01 20:10:37 +0200

    student additional info

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,8 +3,10 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        var student = new Student("John",12345);
-        System.out.println("Hello AppsLab 2020! haa");
-        System.out.println( "name: " + student.name + "number: " + student);
+        var jano = new Student("Jano" , 1,123456,"Legionarska 201001 Zilina");
+        var jaroslav = new Student("Jaroslav",2,578232," Legionarska 101002 Zilina");
+        System.out.println(jano.getNumber() + ", " + jano.getPhone() + ", " + jano.getAddress());
+        System.out.println(jaroslav.getNumber() + ", " + jaroslav.getPhone() + ", " + jaroslav.getAddress());
+
     }
 }
--- src/com/appslab/student.java
+++ src/com/appslab/student.java
@@ -2,10 +2,52 @@ package com.appslab;
 
 class Student
 {
-    public String name;
-    public int number;
-    public Student(String name,int number){
+    private String name;
+    private int number;
+    private int phone;
+    private String address;
+    public Student(String name,int number,int phone,String address)
+    {
         this.name = name;
         this.number = number;
+        setPhone(phone);
+        setAddress(address);
     }
+    public  Student()
+    {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
