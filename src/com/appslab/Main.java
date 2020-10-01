040a5688f14220bdf23fa922c69f6545850769af
z18ista05 <simonheglasik@gmail.com>
2020-10-01 09:28:35 +0200

    student

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,8 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
+        var student = new Student("John",12345);
         System.out.println("Hello AppsLab 2020! haa");
+        System.out.println( "name: " + student.name + "number: " + student);
     }
 }
---
+++ src/com/appslab/student.java
@@ -0,0 +1,11 @@
+package com.appslab;
+
+class Student
+{
+    public String name;
+    public int number;
+    public Student(String name,int number){
+        this.name = name;
+        this.number = number;
+    }
+}
