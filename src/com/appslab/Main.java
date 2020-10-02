c993592080ac1de12b39ec2a7972da22561c5325
simon heglasik <simonheglasik@gmail.com>
2020-10-02 22:44:53 +0200

    budget

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,20 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello AppsLab 2020! haa");
+        Person[] personel = {new Person("Martin", 17,1243), new Person("Karol", 38, 6890), new Person("Adam",46,7023)};
+        System.out.println(budgetSum(personel));
+
+    }
+    public static int budgetSum(Person[] people)
+    {
+        int budget = 0;
+        for (var person:people)
+        {
+            budget += person.getBudget();
+        }
+        return budget;
     }
 }
