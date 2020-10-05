f7d5bea60c1a268af0824a0e9a9850aea49527d6
simon heglasik <simonheglasik@gmail.com>
2020-10-05 19:49:57 +0200

    employee, job class

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,13 @@
 package com.appslab;
 
+import java.sql.Date;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        var programmer = new JobPosition("IT Programmer",2100);
+        var employee = new Employee("Jožko","Mrkvička",new Date(90,7,4),programmer);
+        System.out.println(employee.getName() + "," + employee.getSurname() + "," + employee.getDateofBirth() + "," + employee.getJob().getName() + "," + "salary:" + employee.getJob().getSalary());
     }
 }
