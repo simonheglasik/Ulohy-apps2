bfd6b6b3202b52a600251a189cbb9c06637db39e
simon heglasik <simonheglasik@gmail.com>
2020-10-26 17:29:22 +0100

    BE 1

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,21 @@
 package com.appslab;
 
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        int[] numbers = {1, 1, 1, 2, 2, 3, 4, 5};
+        var list = new ArrayList<Integer>();
+        for(int i = 0 ; i < numbers.length; i++)
+            list.add(numbers[i]);
+        printList(list);
+    }
+    public static void printList(ArrayList<Integer> list)
+    {
+        if(list.isEmpty())
+            System.out.println("list is empty");
+        list.forEach(n -> System.out.println(n));
     }
 }
