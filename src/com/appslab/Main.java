0fcc1e8137b08203eb49250e2cbce0715c1ec67d
simon heglasik <simonheglasik@gmail.com>
2020-10-27 13:12:50 +0100

    BE 3

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,22 @@
 package com.appslab;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello AppsLab 2020! haa");
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < 100;i++)
+            list.add(i);
+        System.out.println(deleteEveryEven(list));
+    }
+
+    public static ArrayList<Integer> deleteEveryEven(ArrayList<Integer> list)
+    {
+        var returnList = new ArrayList<Integer>();
+        returnList = list;
+        returnList.removeIf(n -> n % 2 == 0);
+        return returnList;
     }
 }
