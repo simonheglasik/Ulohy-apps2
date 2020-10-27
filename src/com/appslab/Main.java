83e2e4611e9d9563e604dce1f175eb82d7f428d5
simon heglasik <simonheglasik@gmail.com>
2020-10-27 14:58:35 +0100

    BE 4

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,23 @@
 package com.appslab;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        int[] numbers = {1, 1, 1, 2, 2, 3, 4, 5};
+        var list = new ArrayList<Integer>();
+        for(int i = 0 ; i < numbers.length; i++)
+            list.add(numbers[i]);
+        printEven(list);
+    }
+    public static void printEven(ArrayList<Integer> list)
+    {
+        var returnList = new ArrayList<Integer>();
+        list.forEach(n -> {
+            if(n % 2 == 0)
+                returnList.add(n);
+        });
+        System.out.println(returnList);
     }
 }
