230ada2640e8b330e32388463256b15d293694a6
simon heglasik <simonheglasik@gmail.com>
2020-10-28 15:44:25 +0100

    BE 5

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,15 @@
 package com.appslab;
 
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello AppsLab 2020! haa");
+    }
+    public static void multipleByLenght(ArrayList<Integer> list)
+    {
+        list.stream().forEach(n -> n *= 2);
     }
 }
