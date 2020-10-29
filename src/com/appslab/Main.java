05f23224212303af9a9611500f1888c5641e2012
simon heglasik <simonheglasik@gmail.com>
2020-10-29 15:29:26 +0100

    BE 6

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,19 @@
 package com.appslab;
 
-public class Main {
+import java.util.Arrays;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Person[] people = {new Person("Mario",33,2995) ,new Person("John",24,1899),new Person("Joe",30,4321) };
+        System.out.println(calculateBudget(people));
+    }
+    public static int calculateBudget(Person[] people)
+    {
+        return Arrays.stream(people).map(n -> n.getBudget()).collect(Collectors.summingInt(Integer::intValue));
     }
 }
