2e82ba94d3710b2b8bac3c70e73c53bb9b8683d7
simon heglasik <simonheglasik@gmail.com>
2020-11-06 14:17:52 +0100

    BE 8

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,16 @@
 package com.appslab;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello AppsLab 2020! haa");
+        int[] numbers = {1, 1, 1, 2, 2, 3, 4, 5};
+        var list = new HashSet<Integer>();
+        Arrays.stream(numbers).forEach(n -> list.add(n));
+        System.out.println(list);
     }
 }
