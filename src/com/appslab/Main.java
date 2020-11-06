b2716b83e4a7920b8bb348553965b0e83b79fce3
simon heglasik <simonheglasik@gmail.com>
2020-11-06 14:36:33 +0100

    BE 9

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,16 @@
 package com.appslab;
 
+import java.util.HashMap;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        var map = new HashMap<String,Integer>();
+        map.put("red",1);
+        map.put("green",2);
+        map.put("Black",3);
+        map.put("White",4);
+        System.out.println(map);
     }
 }
